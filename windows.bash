1071b7a3140f5bece27772004f916188f99662cc
y7000p <btian1@umd.edu>
2024-07-15 22:44:51 +0800

    y

--- windows.bash
+++ windows.bash
@@ -1,2 +1,2 @@
 # /bin/bash
-pyinstaller -F --noconsole --add-data "models;models" --add-data "data;data" --collect-all customtkinter --collect-all sklearn --collect-all matplotlib .\diagnosis.py
+pyinstaller --noconsole --add-data "models;models" --add-data "data;data" --collect-all customtkinter --collect-all sklearn --collect-all matplotlib .\diagnosis.py
